5d0986e9cfe8390d2102e1e610ea5f6d923a64cb
busyammonia <159623986+busyammonia@users.noreply.github.com>
2024-02-10 18:25:52 +0300

    feat: add new disko configuration for "pc"

---
+++ disko/pc/disko.nix
@@ -0,0 +1,39 @@
+{ disks, ... }: {
+  disko.devices = {
+    disk = {
+      main = {
+        type = "disk";
+        device = builtins.elemAt disks 0;
+        content = {
+          type = "gpt";
+          efiGptPartitionFirst = false;
+          partitions = {
+            boot = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+            };
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "f2fs";
+                extraArgs = ["extra_attr" "inode_checksum" "sb_checksum" "compression"];
+                mountOptions = ["defaults" "compress_algorithm=zstd:9" "compress_chksum" "atgc" "gc_merge" "lazytime" "checkpoint_merge"];
+                mountpoint = "/nix";
+              };
+            };            
+          };
+        };
+      };
+    };
+  };
+}
