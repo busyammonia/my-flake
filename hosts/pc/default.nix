b1864e8d115d39b8ee9c3b7bab88024479b2f28e
busyammonia <159623986+busyammonia@users.noreply.github.com>
2024-02-11 12:56:57 +0300

    chore: add "rescue" option to "pc" boot parameters

--- hosts/pc/default.nix
+++ hosts/pc/default.nix
@@ -48,7 +48,7 @@
 
   systemd.enableEmergencyMode = true;
   boot.initrd.systemd.emergencyAccess = true;
-  boot.kernelParams = [ "boot.shell_on_fail" "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1" ];
+  boot.kernelParams = [ "rescue" "boot.shell_on_fail" "systemd.setenv=SYSTEMD_SULOGIN_FORCE=1" ];
 
   boot.kernelPackages = pkgs.linuxPackages_cachyos;
   networking.firewall.enable = false;
