265c78e51e4b52afb405deb5cf0721401ab48ed9
busyammonia <159623986+busyammonia@users.noreply.github.com>
2024-02-11 12:49:28 +0300

    chore: add "debug" option to "verbosity" module

--- hosts/common/tweak/common/verbosity.nix
+++ hosts/common/tweak/common/verbosity.nix
@@ -1,7 +1,7 @@
 { config, ... }: {
   boot = {
     kernelParams =
-      [ "udev.log_level=7" "rd.udev.log_level=7" "rd.systemd.show_status=true" ];
+      [ "udev.log_level=7" "debug" "rd.udev.log_level=7" "rd.systemd.show_status=true" ];
     consoleLogLevel = 7;
     initrd.verbose = true;
   };
