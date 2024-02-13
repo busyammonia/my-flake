66476e5966925fe18cfe61b16fdb1959c7065115
busyammonia <159623986+busyammonia@users.noreply.github.com>
2024-02-13 14:18:57 +0300

    wip

--- homes/ye/global/default.nix
+++ homes/ye/global/default.nix
@@ -42,6 +42,7 @@ rec {
   programs.bash = {
     enable = true;
     sessionVariables = {
+      __MYFLAKE1__ = "yes";
       GITHUB_TOKEN = "$(cat ${sops.secrets.github_access_token.path})";
       __MYFLAKE__ = "yes";
     };
